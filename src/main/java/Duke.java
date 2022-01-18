763105caa6aae2da1976def92880d17d4b7a6f6b
ro4k9 <e0552240@u.nus.edu>
2022-01-18 23:23:20 +0900

    Added the ability to store task and updated the list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,20 @@
 import java.util.Scanner;
 
 public class Duke {
-    static String greeting = "Hello! I'm Duke \n What can I do for you?";
+    static String line = "--------------";
+    static String greeting = "Hello! I'm Duke \nWhat can I do for you? \n";
     static String ending = "Bye. Hope to see you again soon!";
+    static int totalTask = 0;
+    static Task[] tasks= new Task[100];
 
     public Duke() {
     }
+
     public static void main(String[] args) {
+        // start the program
+        System.out.println(line);
         System.out.println(greeting);
+        System.out.println(line);
         new Duke().run();
     }
 
@@ -18,12 +25,31 @@ public class Duke {
             String input;
             input = sc.nextLine();
 
+            System.out.println(line);
             if(input.equals("bye")) {
-                System.out.println(ending);
                 exit = true;
+                System.out.println(ending);
+            } else if(input.equals("list")) {
+                for(int i=1; i <= totalTask; i++) {
+                    System.out.println(i+". "+ tasks[i-1].getTask());
+                }
             } else {
-                System.out.println(input);
+                    System.out.println("added: "+ input);
+                    tasks[totalTask] = new Task(input);
+                    totalTask++;
             }
+            System.out.println(line);
         }
+    }
+}
+
+class Task {
+    String task;
+    public Task(String task) {
+        this.task = task;
+    }
+
+    public String getTask() {
+        return task;
     }
 }
