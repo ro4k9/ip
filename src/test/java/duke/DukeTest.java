8ee8f2a986daa80042fcd7d7d0f874941aa6ebc5
ro4k9 <e0552240@u.nus.edu>
2022-02-02 14:25:46 +0900

    Add whitespace and re-order import according to coding standards in java

--- src/test/java/duke/DukeTest.java
+++ src/test/java/duke/DukeTest.java
@@ -1,10 +1,12 @@
 package duke;
-import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 
 public class DukeTest {
     @Test
-    public void createTask(){
+    public void createTask() {
         Todo t = new Todo("test1");
         Event e = new Event("test2", "home");
         Deadline d = new Deadline("test3", "monday");
@@ -13,12 +15,11 @@ public class DukeTest {
         assertEquals("[D][ ] test3 (by: monday)", d.toString());
     }
     @Test
-    public void parseDate(){
+    public void parseDate() {
         Parser p = new Parser();
         assertEquals("Oct 25 2012", p.convertDate("2012-10-25"));
         assertEquals("May 03 2012", p.convertDate("2012-05-03"));
         assertEquals("Jun 25 2012", p.convertDate("2012-06-25"));
         assertEquals("testdate", p.convertDate("testdate"));
     }
-
 }
