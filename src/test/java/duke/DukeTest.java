cbf6e8ab954841c7c5ae7e1e77adfc49d3891a2f
ro4k9 <e0552240@u.nus.edu>
2022-02-04 16:39:36 +0900

    Update build.gradle and delete spacing

--- src/test/java/duke/DukeTest.java
+++ src/test/java/duke/DukeTest.java
@@ -1,12 +1,10 @@
 package duke;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DukeTest {
     @Test
-    public void createTask() {
+    public void createTask(){
         Todo t = new Todo("test1");
         Event e = new Event("test2", "home");
         Deadline d = new Deadline("test3", "monday");
@@ -15,11 +13,12 @@ public class DukeTest {
         assertEquals("[D][ ] test3 (by: monday)", d.toString());
     }
     @Test
-    public void parseDate() {
+    public void parseDate(){
         Parser p = new Parser();
         assertEquals("Oct 25 2012", p.convertDate("2012-10-25"));
         assertEquals("May 03 2012", p.convertDate("2012-05-03"));
         assertEquals("Jun 25 2012", p.convertDate("2012-06-25"));
         assertEquals("testdate", p.convertDate("testdate"));
     }
+
 }
