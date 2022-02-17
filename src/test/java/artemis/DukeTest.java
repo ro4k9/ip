074074d331bf7e9c0b9838d8fcd84f5a4b8fb39a
ro4k9 <e0552240@u.nus.edu>
2022-02-17 23:45:46 +0900

    Improve coding standard for Junit test

--- src/test/java/artemis/DukeTest.java
+++ src/test/java/artemis/DukeTest.java
@@ -2,9 +2,9 @@ package artemis;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Test;
-
 import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
 
 public class DukeTest {
 
@@ -12,7 +12,7 @@ public class DukeTest {
     public void createTask() {
         Todo t = new Todo("test1");
         Event e = new Event("test2", "home");
-        Deadline d = new Deadline("test3", LocalDate.of(2020,2,2));
+        Deadline d = new Deadline("test3", LocalDate.of(2020, 2, 2));
         assertEquals("[T][ ] test1", t.toString());
         assertEquals("[E][ ] test2 (at: home)", e.toString());
         assertEquals("[D][ ] test3 (by: Feb 02 2020)", d.toString());
@@ -22,7 +22,7 @@ public class DukeTest {
     public void markTask() {
         Todo t = new Todo("test4");
         Event e = new Event("test5", "home");
-        Deadline d = new Deadline("test6", LocalDate.of(2020,2,2));
+        Deadline d = new Deadline("test6", LocalDate.of(2020, 2, 2));
         t.markAsDone();
         e.markAsDone();
         d.markAsDone();
@@ -30,13 +30,13 @@ public class DukeTest {
         assertEquals("[E][X] test5 (at: home)", e.toString());
         assertEquals("[D][X] test6 (by: Feb 02 2020)", d.toString());
     }
-     @Test
+
+    @Test
     public void parseDate() {
         LocalDate firstInputDate = Parser.convertDate("2002-10-25");
         LocalDate secondInputDate = Parser.convertDate("2012-05-03");
         LocalDate thirdInputDate = Parser.convertDate("2022-06-25");
-
-       assertEquals("Oct 25 2002", Parser.dateToString(firstInputDate));
+        assertEquals("Oct 25 2002", Parser.dateToString(firstInputDate));
         assertEquals("May 03 2012", Parser.dateToString(secondInputDate));
         assertEquals("Jun 25 2022", Parser.dateToString(thirdInputDate));
     }
